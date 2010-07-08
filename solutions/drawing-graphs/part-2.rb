01da7b87d889ff43290324564f99e31a977f1e74
Manfred Stienstra <manfred@fngtps.com>
2010-07-08 21:32:42 +0200

    Improve styling. Add comments to the solution files.

--- solutions/drawing-graphs/part-2.rb
+++ solutions/drawing-graphs/part-2.rb
@@ -1,8 +1,14 @@
 require 'rubygems'
 require 'png'
 
-public_directory = File.expand_path('../../../public', __FILE__)
+# Determine the full path to the images directory so the result ends up in the exercises document.
+graph_filename = File.expand_path('../../../public/images/drawing-graphs-line.png', __FILE__)
 
+# Create a PNG canvas with a black background
 canvas = PNG::Canvas.new(64, 65, PNG::Color::Black)
+
+# Draw a white line across the image in the center
 canvas.line(0, 32, 63, 32, PNG::Color::White)
-PNG.new(canvas).save(File.join(public_directory, 'images/drawing-graphs-line.png'))
+
+# Write the file to disk
+PNG.new(canvas).save(graph_filename)
--- solutions/drawing-graphs/part-3.rb
+++ solutions/drawing-graphs/part-3.rb
@@ -1,19 +1,32 @@
 require 'rubygems'
 require 'png'
 
+# Define a method that renders integer data to a file
 def render_data(data, image_filename)
   canvas = PNG::Canvas.new(64, 65, PNG::Color::Black)
+  
+  # The y-coordinate of the center is 32
   center = 32
   
+  # Loop over all the data together with the index of the data
+  # we use the the index as the x-coordinate
   data.each_with_index do |value, x|
+    
+    # Scale the values so they fit in our image
     y = center + ((value / 100.0) * 31).round
+    
+    # Draw a line from the center to the value we calculated
     canvas.line(x, center, x, y, PNG::Color::Red)
   end
   
+  # Draw a white line across the center
   canvas.line(0, center, 63, center, PNG::Color::White)
+  
+  # Write the PNG to disk
   PNG.new(canvas).save(image_filename)
 end
 
+# Define a method that renders data from a filename to a file
 def render_file(data_filename, image_filename)
   render_data(
     File.read(data_filename).split("\n").map { |line| line.to_i },
@@ -21,6 +34,8 @@ def render_file(data_filename, image_filename)
   )
 end
 
+# Call the render method with the data filename and a filename in the public directory so
+# the image shows up in the exercises.
 render_file(
   File.expand_path('../../../exercises/drawing-graphs/test/examples/001-sinus.txt', __FILE__),
   File.expand_path('../../../public/images/drawing-graphs-solution.png', __FILE__)
