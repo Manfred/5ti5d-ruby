daa7eba76849fa92070f3c1e6c84b6cdb2223a43
Manfred Stienstra <manfred@fngtps.com>
2011-06-29 22:10:40 +0200

    Small improvements to the second solution.

--- solutions/parsing-http-headers/part-2.rb
+++ solutions/parsing-http-headers/part-2.rb
@@ -1,5 +1,7 @@
 # encoding: utf-8
 
+require 'pp'
+
 # Define a method so we can seperate this part of the code from the main
 # loop at the end of the file.
 def parse_headers(input)
@@ -7,34 +9,29 @@ def parse_headers(input)
   # Instead of each we now use inject to we don't have to initialize the
   # headers hash from outside the loop.
   input.split("\n").inject({}) do |headers, line|
-    parts = line.split(":")
+    # You can assign multiple variables at once when the method returns an array
+    header_name, header_value = line.split(":", 2)
     
     # In the files the headers start with a status line, we need to
     # ignore it so we only use lines which have at least one semicolon.
-    if parts.length > 1
-      header_name = parts[0].downcase
-      header_value = parts[1..-1].join(':').strip
-      headers[header_name] = header_value
+    unless header_value.nil?
+      headers[header_name.downcase] = header_value.strip
     end
+    
     headers
   end
 end
 
 # Determine the full path to the examples directory
-examples_directory = File.expand_path('../../../exercises/parsing-http-headers/test/examples', __FILE__)
+examples_directory = File.expand_path('../../../exercises/parsing-http-headers/examples', __FILE__)
 
 # Loop over all the entries in the examples directory
-Dir.entries(examples_directory).each do |name|
+Dir.glob(File.join(examples_directory, '*.txt')).each do |filename|
+  # Read the file from disk
+  input = File.read(filename)
   
-  # Directory entries also include the . and .. special directories on
-  # UNIX so we need to ignore those.
-  if name =~ /\.txt$/
-    
-    # Read the file from disk
-    input = File.read(File.join(examples_directory, name))
-    
-    # And print the parsed headers to the terminal.
-    puts name
-    p parse_headers(input)
-  end
+  # And print the parsed headers to the terminal
+  puts
+  puts File.basename(filename)
+  pp parse_headers(input)
 end
