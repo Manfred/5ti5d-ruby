3847fd4fb76997ff0f5c3f857a56c6502acebf0c
Manfred Stienstra <manfred@fngtps.com>
2011-07-01 14:44:46 +0200

    Make the examples into valid Ruby.

--- exercises/parsing-http-headers/expected/001-simple.rb
+++ exercises/parsing-http-headers/expected/001-simple.rb
@@ -1,1 +1,1 @@
-{"content-type"=>"text/html", "server"=>"Apache/2.2.14 (Unix)", "date"=>"Sun, 25 Apr 2010 12:17:38 GMT", "content-length"=>"44"}
+Expected.headers = {"content-type"=>"text/html", "server"=>"Apache/2.2.14 (Unix)", "date"=>"Sun, 25 Apr 2010 12:17:38 GMT", "content-length"=>"44"}
