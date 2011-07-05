c70bc5f73d1778876abaa0bf5ec5995c5a094cbb
Manfred Stienstra <manfred@fngtps.com>
2011-07-05 11:45:38 +0200

    Add a solution for part 4 of the web app exercise.

---
+++ solutions/web-app/part-4.rb
@@ -0,0 +1,37 @@
+require 'rubygems'
+require 'sinatra'
+require 'erb'
+require 'datamapper'
+
+configure do
+  set :views, File.expand_path('../templates-part-4', __FILE__)
+  
+  DataMapper::setup(:default, "sqlite3://#{File.expand_path('../part-4.db', __FILE__)}")
+end
+
+class Message
+  include DataMapper::Resource
+  property :id, Serial
+  property :body, Text
+  property :created_at, DateTime
+  
+  validates_presence_of :body, :message => "Please type a message"
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
+
+get '/' do
+  @message = Message.new
+  @messages = Message.all(:order => [:created_at.desc])
+  erb :index
+end
+
+post '/' do
+  @message = Message.new(params[:message])
+  if @message.save
+    redirect to('/')
+  else
+    erb :index
+  end
+end
